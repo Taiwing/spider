b853b97b3ebaf8050d0e06b9b0ce9c9e40790211
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-10-18 14:14:36 +0200

    feat: set defaults for level and path options

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use std::path::PathBuf;
 use clap::{Parser};
 
 #[derive(Parser)]
-#[command(author, version, about)]
+#[command(author, version, about = "Extract every image from a given URL")]
 struct Cli {
+	/// URL to download the images from
 	url: String,
+	/// Recursively download the images in a URL received as a parameter
 	#[arg(short, long)]
 	recursive: bool,
-	#[arg(short, long, value_name = "N")]
+	/// Set the maximum recursion level
+	#[arg(short, long, value_name = "N", default_value = "5")]
 	level: Option<u32>,
-	#[arg(short, long, value_name = "PATH")]
+	/// Path where the images will be saved
+	#[arg(short, long, value_name = "PATH", default_value = "./data")]
 	path: Option<PathBuf>,
 }
 
@@ -17,6 +21,6 @@ fn main() {
 	let args = Cli::parse();
 	println!("url: {}", args.url);
 	println!("recursive: {}", args.recursive);
-	println!("level: {}", args.level.unwrap_or(0));
-	println!("path: {}", args.path.unwrap_or(PathBuf::from("default")).display());
+	println!("level: {}", args.level.unwrap());
+	println!("path: {}", args.path.unwrap().display());
 }
