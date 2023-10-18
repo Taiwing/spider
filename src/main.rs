63870161f0cf154ef37c63998c5d2760dadad3d3
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-10-18 12:49:21 +0200

    feat: use clap to parse options and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+use clap::{Parser};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+	url: String,
+	#[arg(short, long)]
+	recursive: bool,
+	#[arg(short, long, value_name = "N")]
+	level: Option<u32>,
+	#[arg(short, long, value_name = "PATH")]
+	path: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let args = Cli::parse();
+	println!("url: {}", args.url);
+	println!("recursive: {}", args.recursive);
+	println!("level: {}", args.level.unwrap_or(0));
+	println!("path: {}", args.path.unwrap_or(PathBuf::from("default")).display());
 }
